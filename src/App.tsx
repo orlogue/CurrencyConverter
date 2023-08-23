9f581c8826a1f545705f91026020cec1198b3ac5
orlogue <102894674+orlogue@users.noreply.github.com>
2023-08-23 21:49:45 +0700

    Attach views to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Converter from "./views/Converter/Converter.tsx";
+import Costs from "./views/Costs/Costs.tsx";
+import History from "./views/History/History.tsx";
 import './App.module.scss'
 
 function App() {
 
   return (
     <main>
+      <Converter/>
+      <Costs/>
+      <History/>
     </main>
   )
 }
