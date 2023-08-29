3c663bea543d9190052c1b289e714f1acf521b35
orlogue <102894674+orlogue@users.noreply.github.com>
2023-08-29 21:51:57 +0700

    Add hiding logic to section

---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,38 @@
+import styles from "./Section.module.scss";
+import React, { useState } from "react";
+import Title from "../Title/Title.tsx";
+
+interface SideSectionProps {
+  title: string,
+  parentClasses: string,
+  canBeHidden?: boolean,
+  hidden?: boolean,
+  rotateBar?: boolean,
+  handleButtonClick?: () => void,
+  children: React.ReactNode
+}
+
+export default function Section({ title, parentClasses, canBeHidden = false, rotateBar = false, children }: SideSectionProps) {
+  const [hidden, setHidden] = useState(false);
+  const [rotate, setRotate] = useState(rotateBar);
+
+  function handleButtonClick() {
+    setHidden(!hidden);
+    setRotate(!rotate)
+  }
+
+  return (
+    <section className={parentClasses + (hidden ? ' ' + styles.hidden : '')}>
+      <div className={styles['section-bar'] + ' ' + (hidden ? styles.hidden : '') + ' ' + (rotateBar ? styles.rotate : '')}>
+        {canBeHidden &&
+          <img className={hidden ? styles['hide-svg_closed'] : styles['hide-svg']}
+               onClick={handleButtonClick}
+               src={'src/assets/arrow-down.svg'}
+               alt='hide button'/>
+        }
+        <Title title={title}/>
+      </div>
+      {!hidden && children}
+    </section>
+  );
+}
