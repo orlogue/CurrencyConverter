549d9afbccf1aa18afce13b93f10b9f4adb6e1f6
orlogue <102894674+orlogue@users.noreply.github.com>
2023-08-29 21:54:42 +0700

    Create rates plot

--- src/views/History/History.tsx
+++ src/views/History/History.tsx
@@ -1,12 +1,91 @@
 import styles from './History.module.scss';
-import Section from "../../components/ui/Section/Section.tsx";
+import Section from "../../components/Section/Section.tsx";
+import { useCurrency } from "../../context/CurrencyContext.tsx";
+import { useEffect, useState } from "react";
+
+interface Rate {
+  date: string,
+  rate: number
+}
+
+function MinMax(rates: Rate[]): [min: number, max: number] {
+  const min = Math.min(...rates.map(item => item.rate))
+  const max = Math.max(...rates.map(item => item.rate))
+  return [min, max]
+}
 
 export default function History() {
+  const { baseCurrency, targetCurrency, historicalRates } = useCurrency();
+
+  const [rates, setRates] = useState<Rate[]>([]);
+
+  const [min, max] = MinMax(rates);
+  const average = (max + min) / 2
+
+  function GenerateYValues() {
+    const precision = max >= 1 ? 2 : 5;
+    return (
+      <>
+        <div>{max.toFixed(precision)}</div>
+        <div>{((max + average) / 2).toFixed(precision)}</div>
+        <div>{average.toFixed(precision)}</div>
+        <div>{((min + average) / 2).toFixed(precision)}</div>
+        <div>{min.toFixed(precision)}</div>
+      </>
+    );
+  }
+
+  useEffect(() => {
+    if (historicalRates && baseCurrency && targetCurrency && baseCurrency?.code in historicalRates) {
+      const temp: Rate[] = []
+      for (const [date, rates] of Object.entries(historicalRates[baseCurrency.code])) {
+        temp.push({
+          date: date,
+          rate: rates[targetCurrency.code],
+        });
+      }
+      setRates(temp);
+    }
+  }, [historicalRates, baseCurrency, targetCurrency]);
+
   return (
     <Section
       parentClasses={styles.history}
       title="History"
+      canBeHidden={true}
+      rotateBar={true}
     >
+      <div className={styles.description}>
+        <div>Rates for the last 10 days</div>
+        <div>{baseCurrency?.code} to {targetCurrency?.code}</div>
+      </div>
+      <div className={styles.plot}>
+        <div className={styles.plot__body}>
+          <div className={styles['y-axis']}>
+            {GenerateYValues()}
+          </div>
+          <div className={styles.plot__column}>
+            <div className={styles.rates} id="rates">
+              {rates.map((rate, i) => {
+                const maxHeight = 200 - 12 + 1.5;
+                const minHeight = 10 + 12.5 + 1.5;
+                const subHeight = maxHeight - minHeight;
+                const newMax = max - min;
+                const coefficient = rate.rate - min
+                return (
+                  <div className={styles.col} key={i} style={{
+                  height: `${minHeight + subHeight / newMax * coefficient}px`
+                }}></div>);
+              })}
+            </div>
+            <div className={styles['x-axis']}>
+              {rates.map(rate => {
+                return <div key={rate.date}>{rate.date.slice(-2)}</div>
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </Section>
   );
 }
