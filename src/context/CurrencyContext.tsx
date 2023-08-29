d6f81678e556a558fc4c817e8bed4ef403fbb585
orlogue <102894674+orlogue@users.noreply.github.com>
2023-08-29 21:42:31 +0700

    Restructure and update CurrencyContext and CurrencyProvider

---
+++ src/context/CurrencyContext.tsx
@@ -0,0 +1,154 @@
+import { useEffect, createContext, useContext, useReducer, useCallback } from "react";
+import axios from "axios";
+import {
+  Currencies,
+  CurrencyActions,
+  CurrencyContext,
+  CurrencyRate,
+  CurrencyRates,
+  CurrencyState,
+  HistoricalRate
+} from "./CurrencyTypes.tsx";
+
+export const context = createContext<CurrencyContext>({} as CurrencyContext);
+
+export function useCurrencySource(): CurrencyContext {
+  const [{ baseCurrency, targetCurrency, currencies, currencyRates, historicalRates }, dispatch]
+    = useReducer((state: CurrencyState, action: CurrencyActions): CurrencyState => {
+    switch (action.type) {
+      case "setBaseCurrency": {
+        return { ...state, baseCurrency: action.payload };
+      }
+      case "setTargetCurrency": {
+        return { ...state, targetCurrency: action.payload };
+      }
+      case "setCurrencies": {
+        return { ...state, currencies: action.payload };
+      }
+      case "setCurrencyRates": {
+        return {
+          ...state,
+          currencyRates: {
+            ...state.currencyRates,
+            [action.code]: action.rates
+          }
+        };
+      }
+      case "setHistoricalRates": {
+        return {
+          ...state,
+          historicalRates: {
+            ...state.historicalRates,
+            [action.code]: action.rates
+          }
+        };
+      }
+      default: {
+        return {} as CurrencyContext
+      }
+    }
+  }, {
+    baseCurrency: undefined,
+    targetCurrency: undefined,
+    currencies: {} as Currencies,
+    currencyRates: {} as CurrencyRates,
+    historicalRates: undefined
+  });
+
+  useEffect(() => {
+    axios.get<{
+      data: Currencies
+    }>(`${import.meta.env.VITE_API_BASE_URL}/currencies?apikey=${import.meta.env.VITE_API_KEY}`)
+      .then(response => {
+        dispatch({
+          type: "setCurrencies",
+          payload: response.data.data
+        });
+        dispatch({
+          type: "setBaseCurrency",
+          payload: response.data.data.RUB
+        });
+        dispatch({
+          type: "setTargetCurrency",
+          payload: response.data.data.USD
+        });
+        getHistoricalRates("RUB");
+        getHistoricalRates("USD");
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const getCurrencyRates = (code: string) => {
+    if (code in currencyRates) return;
+
+    axios.get<{
+      data: CurrencyRate
+    }>(`${import.meta.env.VITE_API_BASE_URL}/latest?apikey=${import.meta.env.VITE_API_KEY}&base_currency=${code}`)
+      .then(res =>
+        dispatch({
+          type: "setCurrencyRates",
+          code: code,
+          rates: res.data.data,
+        }))
+      .catch(err => console.log(err))
+  }
+
+  const getHistoricalRates = (code: string) => {
+    if (historicalRates && code in historicalRates) return;
+
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+    const yesterday = date.toISOString().split('T')[0];
+    date.setDate(date.getDate() - 9);
+    const tenDaysAgo = date.toISOString().split('T')[0];
+
+    axios.get<{
+      data: HistoricalRate
+    }>(`${import.meta.env.VITE_API_BASE_URL}/historical?apikey=${import.meta.env.VITE_API_KEY}&base_currency=${code}&date_from=${tenDaysAgo}&date_to=${yesterday}`)
+      .then(res =>
+        dispatch({
+          type: "setHistoricalRates",
+          code: code,
+          rates: res.data.data,
+        }))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    if (baseCurrency) {
+      getCurrencyRates(baseCurrency.code)
+    }
+    if (targetCurrency) {
+      getCurrencyRates(targetCurrency.code)
+    }
+  }, [baseCurrency, targetCurrency]);
+
+  useEffect(() => {
+    if (baseCurrency) {
+      getHistoricalRates(baseCurrency.code)
+    }
+    if (targetCurrency) {
+      getHistoricalRates(targetCurrency.code)
+    }
+  }, [baseCurrency, targetCurrency])
+
+  const setBaseCurrency = useCallback((code: string) => {
+    dispatch({
+      type: "setBaseCurrency",
+      payload: currencies[code]
+    });
+  }, [currencies]);
+
+  const setTargetCurrency = useCallback((code: string) => {
+    dispatch({
+      type: "setTargetCurrency",
+      payload: currencies[code]
+    });
+  }, [currencies]);
+
+  return { baseCurrency, targetCurrency, currencies, currencyRates, historicalRates, setBaseCurrency, setTargetCurrency };
+}
+
+export function useCurrency() {
+  return useContext(context)
+}
---
+++ src/context/CurrencyTypes.tsx
@@ -0,0 +1,37 @@
+export type CurrencyRate = Record<string, number>
+export type CurrencyRates = Record<string, CurrencyRate>
+
+export type Currencies = Record<string, Currency>
+
+export type HistoricalRate = Record<string, CurrencyRate>
+export type HistoricalRates = Record<string, HistoricalRate>
+
+export interface Currency {
+  symbol: string,
+  name: string,
+  symbol_native: string,
+  decimal_digits: number,
+  rounding: number,
+  code: string,
+  name_plural: string,
+}
+
+export interface CurrencyState {
+  baseCurrency: Currency | undefined,
+  targetCurrency: Currency | undefined,
+  currencies: Currencies,
+  currencyRates: CurrencyRates,
+  historicalRates: HistoricalRates | undefined,
+}
+
+export type CurrencyActions =
+  | { type: "setBaseCurrency", payload: Currency }
+  | { type: "setTargetCurrency", payload: Currency }
+  | { type: "setCurrencies", payload: Currencies }
+  | { type: "setCurrencyRates", code: string, rates: CurrencyRate }
+  | { type: "setHistoricalRates", code: string, rates: HistoricalRate }
+
+export interface CurrencyContext extends CurrencyState {
+  setBaseCurrency: (code: string) => void,
+  setTargetCurrency: (code: string) => void,
+}
